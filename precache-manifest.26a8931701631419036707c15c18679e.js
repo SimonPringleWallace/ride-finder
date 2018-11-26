3e5a1586e64ba2af3a811b23b7dc5aa1a3817135
SimonPringleWallace <s.pringlewallace@gmail.com>
2018-11-26 10:22:06 -0500

    deploy task

---
+++ precache-manifest.26a8931701631419036707c15c18679e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdf4e3d0de19a824a756dcf0a0dd5e30",
+    "url": "/ride-finder/static/media/bike80.cdf4e3d0.jpg"
+  },
+  {
+    "revision": "907810a14ab6aa719fe3",
+    "url": "/ride-finder/static/js/runtime~main.907810a1.js"
+  },
+  {
+    "revision": "b31445502d70a8c0a0f5",
+    "url": "/ride-finder/static/js/main.b3144550.chunk.js"
+  },
+  {
+    "revision": "7c8a08d6b0e4fc199be6",
+    "url": "/ride-finder/static/js/1.7c8a08d6.chunk.js"
+  },
+  {
+    "revision": "b31445502d70a8c0a0f5",
+    "url": "/ride-finder/static/css/main.1e5d6683.chunk.css"
+  },
+  {
+    "revision": "324b759a26047623235978a17f17ad28",
+    "url": "/ride-finder/index.html"
+  }
+];
